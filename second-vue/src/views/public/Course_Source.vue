63030e7800d5083760b72ee46fad634cbb5dae43
admin <qq1648422868@gmail.com>
2021-06-06 16:31:15 +0800

    update

---
+++ second-vue/src/views/public/Course_Source.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "Course_Source"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ second-vue/src/views/public/Course_Video.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Course_Video"
+}
+</script>
+
+<style scoped>
+
+</style>
